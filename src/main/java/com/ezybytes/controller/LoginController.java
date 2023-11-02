7c599ed1f1d2fb10cb494c2c98b352508bc325ff
Pavlo Ilnitskyi <inavlo092@gmail.com>
2023-11-02 18:49:05 +0200

    Create LoginService

--- src/main/java/com/ezybytes/controller/LoginController.java
+++ src/main/java/com/ezybytes/controller/LoginController.java
@@ -2,6 +2,7 @@ package com.ezybytes.controller;
 
 import com.ezybytes.model.Customer;
 import com.ezybytes.repository.CustomerRepository;
+import com.ezybytes.service.LoginService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,28 +14,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 @Controller
 public class LoginController {
     @Autowired
-    CustomerRepository  customerRepository;
-    @Autowired
-    private PasswordEncoder  passwordEncoder;
+    LoginService loginService;
+
     @PostMapping("/register")
     public ResponseEntity<String> registerUser(@RequestBody Customer customer) {
-        Customer savedCustomer = null;
-        ResponseEntity<String> response = null;
-        try {
-            String hashPwd = passwordEncoder.encode(customer.getPwd());
-            customer.setPwd(hashPwd);
-            savedCustomer = customerRepository.save(customer);
-            if (savedCustomer.getId() > 0) {
-                response = ResponseEntity
-                        .status(HttpStatus.CREATED)
-                        .body("Given user details are successfully registered");
-            }
-        } catch (Exception ex) {
-            response = ResponseEntity
-                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body("An exception occured due to " + ex.getMessage());
-        }
-        return response;
+      return loginService.registerUser(customer);
     }
 
 }
